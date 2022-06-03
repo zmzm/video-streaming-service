611b5d2b821701ed8ae8eb3975acd66456332bc0
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-06-03 19:21:03 +0300

    added video endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
+const fs = require('fs');
+const path = require('path');
+
 const limiter = require('./rateLimit');
 
 const app = express();
@@ -10,6 +13,37 @@ app.use(express.json());
 
 app.get('/', (_, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/video', (req, res) => {
+  const { range } = req.headers;
+
+  if (!range) {
+    res.status(400).send('Requires Range header');
+  }
+
+  const videoPath = path.join(__dirname, 'your-video.mp4');
+  const videoSize = fs.statSync(videoPath).size;
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, ''));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  const contentLength = end - start + 1;
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': 'video/mp4',
+  };
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers);
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res);
 });
 
 app.use((req, res, next) => {
